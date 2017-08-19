17eff645f4db5da6cab89e5af75aed65ec39f02d
Andrew Noyes <anoyes34@gmail.com>
2017-08-19 15:32:33 -0700

    Snapshot testing

--- src/config.py
+++ src/config.py
@@ -1,3 +1,19 @@
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
+import os
+
+if os.environ.get('ENV') == 'prod':
+    assert 'DATABASE_URL' in os.environ
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
+    DEBUG = True
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+def get_pretty_config():
+    import json
+    return json.dumps(
+        {k: v
+         for (k, v) in globals().items() if k.isupper()},
+        sort_keys=True,
+        indent=2)
