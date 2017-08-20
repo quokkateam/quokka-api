68203478db0409f5601c5cffe45c5a34ff6ce266
Andrew Noyes <anoyes34@gmail.com>
2017-08-20 15:10:27 -0700

    Allow users to be created without passwords
    Complete with tests and input validation.
    
    Also, fix account enumeration via timing attack by introducing a dummy
    hash. I tested that my tests fail if I remove the can_mint_token flag.

--- src/api_test.py
+++ src/api_test.py
@@ -90,3 +90,50 @@ def test_create_user_flow(client, mocker):
     db.session.commit()
     db.drop_all()
 
+def test_create_user_no_password(client, mocker):
+    db.create_all()
+
+    mocker.patch('email_client.send_verification_email')
+
+    res = client.post('/api/users/', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu')))
+
+    [user] = User.query.all()
+    assert not user.email_verified
+    assert 'id=1' in str(user)
+
+    assert res.json == dict(id=1, email='e@mail.edu')
+    assert res.status_code == 201
+
+    assert email_client.send_verification_email.called
+    (user,), _ = email_client.send_verification_email.call_args
+
+    res = client.post('/api/mint_token', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu')))
+    assert res.status_code == 400
+
+    res = client.post('/api/mint_token', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu', password=[])))
+    assert res.status_code == 400
+
+    res = client.post('/api/mint_token', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu', password='')))
+    assert res.json['reason'] == 'Unrecognized credentials'
+    assert res.status_code == 401
+
+    res = client.post('/api/mint_token', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu', password='hunter2')))
+    assert res.json['reason'] == 'Unrecognized credentials'
+    assert res.status_code == 401
+
+    # Dummy hashed_pw in handler for this route is a hash of 'pw'. Test it.
+    res = client.post('/api/mint_token', headers={'Content-Type': 'application/json'},
+                      data=json.dumps(dict(email='e@mail.edu', password='pw')))
+    assert res.json['reason'] == 'Unrecognized credentials'
+    assert res.status_code == 401
+
+    res = client.post('/api/verify_email/%d/%s' % (user.id, user.email_verification_secret))
+    assert res.status_code == 200
+
+    db.session.commit()
+    db.drop_all()
