31e9912a6ef16c13804d37d6334ca7b912814a10
Ben Whittle <benwhittle31@gmail.com>
2017-08-19 17:51:48 -0700

    GET /schools route set up

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,4 +1,9 @@
+import json
 from flask_restplus import Api
 
 api = Api(version='0.1', title='Quokka API')
 namespace = api.namespace('api')
+
+
+def json_response(data={}):
+  return api.make_response(json.dumps(data))
