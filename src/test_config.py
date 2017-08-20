15e4e9a36488d7691ae106e037a44f4094f21d0b
Andrew Noyes <anoyes34@gmail.com>
2017-08-19 18:28:04 -0700

    Use remote postgres for tests

--- src/test_config.py
+++ src/test_config.py
@@ -1,3 +1,11 @@
+import os
+import subprocess
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+if 'TEST_DB_URL' in os.environ:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DB_URL')
+else:
+    SQLALCHEMY_DATABASE_URI = subprocess.check_output(
+            "heroku config --app quokka-api-test-db | grep DATABASE_URL | awk '{print $2}'",
+            shell=True).decode('utf-8')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
