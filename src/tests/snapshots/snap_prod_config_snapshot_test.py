38ecc8ae1792f8bdc956905aa0eb54afa05a84ad
Ben Whittle <benwhittle31@gmail.com>
2017-08-30 12:54:41 -0700

    removing extra spaces in snapshot

--- src/tests/snapshots/snap_prod_config_snapshot_test.py
+++ src/tests/snapshots/snap_prod_config_snapshot_test.py
@@ -8,7 +8,7 @@ from snapshottest import Snapshot
 snapshots = Snapshot()
 
 snapshots['test_prod_config 1'] = '''{
-  "DEBUG": false, 
-  "SQLALCHEMY_DATABASE_URI": "the-db-url", 
+  "DEBUG": false,
+  "SQLALCHEMY_DATABASE_URI": "the-db-url",
   "SQLALCHEMY_TRACK_MODIFICATIONS": false
 }'''
