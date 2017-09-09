a821401dbbe2f0c18c14d072cbe7475d922a4401
Ben Whittle <benwhittle31@gmail.com>
2017-09-08 23:10:03 -0700

    filling out exercise section and adding defaultIndex

--- src/challenges.py
+++ src/challenges.py
@@ -1,5 +1,6 @@
 challenges = {
   'socializing': {
+    'defaultIndex': 0,
     'name': 'Socializing',
     'overview': [
       {
@@ -10,6 +11,7 @@ challenges = {
     'extra_info': ''
   },
   'good-deeds': {
+    'defaultIndex': 1,
     'name': 'Good Deeds',
     'overview': [
       {
@@ -20,6 +22,7 @@ challenges = {
     'extra_info': ''
   },
   'sleep': {
+    'defaultIndex': 2,
     'name': 'Sleep',
     'overview': [
       {
@@ -30,6 +33,7 @@ challenges = {
     'extra_info': ''
   },
   'healthy-eating': {
+    'defaultIndex': 3,
     'name': 'Healthy Eating',
     'overview': [
       {
@@ -40,6 +44,7 @@ challenges = {
     'extra_info': ''
   },
   'journaling': {
+    'defaultIndex': 4,
     'name': 'Journaling',
     'overview': [
       {
@@ -50,16 +55,34 @@ challenges = {
     'extra_info': ''
   },
   'exercise': {
+    'defaultIndex': 5,
     'name': 'Exercise',
     'overview': [
       {
-        'title': '',
-        'text': ''
+        'title': 'What\'s the science say?',
+        'text': 'Regular physical activity is important for increasing energy and focus, strengthening bones and muscles, and increasing longevity. It\'ll also help you manage stress and anxiety and reduce your risk of cardiovascular disease and obesity. Even our perceptions and mindsets on how physically active we are can predict health and longevity in the domains of stress, diet, and obesity.'
+      },
+      {
+        'title': 'How much exercise should I get?',
+        'text': 'The Physical Activity Guidelines for Americans recommends 2.5 hours per week of moderate-intensity aerobic activity (or 75 minutes of more vigorous exercise) for good general health. Depending on how intense the aerobic activity, you could take a brisk 30-min walk 5x a week or play a high-intensity sport for a shorter period. Adding in strength training twice a week can help with weight loss and increase your resting metabolic rate on top of all this!'
+      },
+      {
+        'title': 'What are some easy things to do?',
+        'text': 'There are easy ways to incorporate physical activity in your day-to-day. Sitting for prolonged periods throughout the day has been linked to heart disease and diabetes, and stepping away from the chair or even just standing for a few minutes every hour can help reduce these negative effects. Choosing to walk or bike, taking the stairs, and cleaning your room are easy ways to incorporate more physical activity in your day.'
+      },
+      {
+        'title': 'Should I focus on strength or cardio?',
+        'text': 'It\'s important to do both! Strength training is important for building muscles, preventing injury, and improving your metabolism. Cardio (aerobic exercise that elevates your heart rate) improves heart health, helps with weight loss, and increases your metabolism, too.'
+      },
+      {
+        'title': 'If you\'re getting bored, switch it up!',
+        'text': 'Try mixing up your workout--varying your exercise routines will help you avoid plateaus in workout performance and also improve adherence. Figuring out what activities get you most excited and motivated will be key to getting consistent physical activity.'
       }
     ],
-    'extra_info': ''
+    'extra_info': 'If circumstances prevent you from fulfilling our outlined challenges, [mind-body exercises may be helpful alternatives to managing a variety of health conditions such as anxiety, insomnia, or other chronic pain symptoms](https://nccih.nih.gov/health/stress/relaxation.htm). The information on this site is for general purposes only and is not intended or implied to be a substitute for professional medical advice, diagnosis, or treatment. Seek advice from your health care provider for your personal health needs.'
   },
   'group-selected-challenge': {
+    'defaultIndex': 6,
     'name': 'Group-Selected Challenge',
     'overview': [
       {
@@ -70,6 +93,7 @@ challenges = {
     'extra_info': ''
   },
   'positivity-mindfulness': {
+    'defaultIndex': 7,
     'name': 'Positivity & Mindfulness',
     'overview': [
       {
