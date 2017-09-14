cfd1011b1de3cf5185b02524130b3f101c486096
Ben Whittle <benwhittle31@gmail.com>
2017-09-13 22:09:37 -0700

    removing unnecessary line importing models into manager

--- manage.py
+++ manage.py
@@ -23,9 +23,6 @@ from flask_script import Manager
 migrate = Migrate(app, db)
 manager = Manager(app)
 
-# Import all models here to make our schema changes available to Migrate
-from src.models import User, Token, School
-
 manager.add_option("-c", "--config", dest="config_module", required=False)
 manager.add_command('db', MigrateCommand)
 
