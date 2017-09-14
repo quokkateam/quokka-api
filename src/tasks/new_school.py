f0f908aa100b972fe67be76015092079dcbae972
Ben Whittle <benwhittle31@gmail.com>
2017-09-13 18:07:24 -0700

    adding CheckIn and CheckInQuestion population to new_school.py task

--- src/tasks/new_school.py
+++ src/tasks/new_school.py
@@ -1,7 +1,7 @@
 import sys
 from argparse import ArgumentParser
-from src.models import School, Challenge
-from src.dbi import create, find_one, find_all
+from src.models import School, Challenge, CheckIn, CheckInQuestion
+from src.dbi import create, find_one, find_all, update
 from src.challenges import universal_challenge_info
 from operator import itemgetter
 from datetime import datetime, timedelta
@@ -19,39 +19,76 @@ if __name__ == '__main__':
     print 'A name is required for the new school. Use the --name argument.'
     exit(0)
 
-  if not args.domains:
-    print 'Domains are required for a school. Use the --domains argument and pass a comma delimited string'
-    exit(0)
-
-  domains = [d.strip() for d in args.domains.split(',') if d.strip() != '']
-
   school = find_one(School, {'name':  args.name})
 
+  domains = None
+  if args.domains:
+    domains = [d.strip() for d in args.domains.split(',') if d.strip() != '']
+
   if school:
-    print 'School named {} already exists. Checking challenges...'.format(args.name)
+    print 'School named {} already exists.'.format(args.name)
+
+    if domains:
+      school = update(school, {'domains': domains})
   else:
     print 'Creating school, {}...'.format(args.name)
+
+    if not domains:
+      print 'Domains are required to create a new school. Use the --domains argument.'
+      exit(0)
+
     school = create(School, {'name': args.name, 'domains': domains})
 
   sorted_challenges = sorted(universal_challenge_info.values(), key=itemgetter('defaultIndex'))
 
   challenges = find_all(Challenge, {'school': school})
 
-  if len(challenges) == len(sorted_challenges):
-    print 'Challenges already created for school.'
-    exit(0)
-
   print 'Creating challenges for school...'
 
+  # For each challenge listed in challenges.py...
   for c in sorted_challenges:
     start_date = kickoff_date + timedelta(days=(7 * c['defaultIndex']))
     end_date = start_date + timedelta(days=6)
 
-    create(Challenge, {
-      'name': c['name'],
-      'school': school,
-      'start_date': start_date,
-      'end_date': end_date
-    })
+    # Find or create a Challenge
+    challenge = find_one(Challenge, {'name': c['name'], 'school': school})
+
+    if not challenge:
+      print 'Creating Challenge: {}...'.format(c['name'])
+
+      challenge = create(Challenge, {
+        'name': c['name'],
+        'school': school,
+        'start_date': start_date,
+        'end_date': end_date
+      })
+
+    # Find or create a new CheckIn for this Challenge
+    check_in = find_one(CheckIn, {'challenge': challenge})
+
+    if not check_in:
+      print 'Creating CheckIn for Challenge: {}...'.format(c['name'])
+      check_in = create(CheckIn, {'challenge': challenge})
+
+    # Find or create new CheckInQuestions for this CheckIn
+    questions = c['check_in_questions']
+
+    i = 0
+    for q in questions:
+      check_in_question = find_one(CheckInQuestion, {
+        'check_in': check_in,
+        'text': q
+      })
+
+      if not check_in_question:
+        print 'Creating CheckInQuestion...'
+
+        create(CheckInQuestion, {
+          'check_in': check_in,
+          'text': q,
+          'order': i
+        })
+
+      i += 1
 
   print 'Done!'
