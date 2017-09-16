4d17e5700a1528953a944d81f5a997b86d2b5d5b
Ben Whittle <benwhittle31@gmail.com>
2017-09-15 21:59:24 -0700

    switching default order of challenges; adding icon urls for all

--- src/challenges.py
+++ src/challenges.py
@@ -2,7 +2,7 @@ universal_challenge_info = {
   'socializing': {
     'defaultIndex': 0,
     'name': 'Socializing',
-    'icon': '',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/socialize-green.png',
     'overview': [
       {
         'title': '',
@@ -14,108 +14,48 @@ universal_challenge_info = {
     'check_in_questions': [],
     'preview_text': 'Social support plays an important role in our behaviors, interactions, self-image, and how we respond to stress.'
   },
-  'good-deeds': {
-    'defaultIndex': 1,
-    'name': 'Good Deeds',
-    'icon': '',
-    'overview': [
-      {
-        'title': '',
-        'content': ''
-      }
-    ],
-    'links': [],
-    'extra_info': '',
-    'check_in_questions': [],
-    'preview_text': 'A positive feedback loop exists between kindness and happiness &mdash; the more you do to make other people feel better, the more your own happiness increases!'
-  },
-  'sleep': {
-    'defaultIndex': 2,
-    'name': 'Sleep',
-    'icon': '',
-    'overview': [
-      {
-        'title': '',
-        'content': ''
-      }
-    ],
-    'links': [],
-    'extra_info': '',
-    'check_in_questions': [],
-    'preview_text': 'We all underestimate how big of an impact getting a good night\'s sleep can have on our overall health and happiness.'
-  },
-  'healthy-eating': {
-    'defaultIndex': 3,
-    'name': 'Healthy Eating',
-    'icon': '',
-    'overview': [
-      {
-        'title': '',
-        'content': ''
-      }
-    ],
-    'links': [],
-    'extra_info': '',
-    'check_in_questions': [],
-    'preview_text': 'Healthy eating has multiple benefits for you. It can help you stay in shape, increase your energy, and boost your immune system.'
-  },
-  'journaling': {
-    'defaultIndex': 4,
-    'name': 'Journaling',
-    'icon': '',
-    'overview': [
-      {
-        'title': '',
-        'content': ''
-      }
-    ],
-    'links': [],
-    'extra_info': '',
-    'check_in_questions': [],
-    'preview_text': 'Physically logging your days and putting your thoughts on paper can be great for organizing thoughts.'
-  },
   'exercise': {
-    'defaultIndex': 5,
+    'defaultIndex': 1,
     'name': 'Exercise',
     'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/running-green.png',
     'overview': [
       {
         'title': 'What\'s the science say?',
         'content': """Regular physical activity is important for increasing energy and focus, strengthening bones and muscles, 
-          and increasing longevity [[1]](https://www.cdc.gov/physicalactivity/basics/pa-health/index.htm#StrengthenBonesMuscle). 
-          It'll also help you manage stress and anxiety and reduce your risk of cardiovascular disease and obesity. 
-          Even our perceptions and mindsets on how physically active we are can predict health and longevity in the 
-          domains of stress, diet, and obesity [[2]](https://news.stanford.edu/2017/07/20/self-perceptions-linked-shorter-lifespans)."""
+        and increasing longevity [[1]](https://www.cdc.gov/physicalactivity/basics/pa-health/index.htm#StrengthenBonesMuscle). 
+        It'll also help you manage stress and anxiety and reduce your risk of cardiovascular disease and obesity. 
+        Even our perceptions and mindsets on how physically active we are can predict health and longevity in the 
+        domains of stress, diet, and obesity [[2]](https://news.stanford.edu/2017/07/20/self-perceptions-linked-shorter-lifespans)."""
       },
       {
         'title': 'How much exercise should I get?',
         'content': """The Physical Activity Guidelines for Americans recommends 2.5 hours per week of moderate-intensity 
-          aerobic activity (or 75 minutes of more vigorous exercise) for good general health. Depending on how intense the 
-          aerobic activity, you could take a brisk 30-min walk 5x a week or play a high-intensity sport for a shorter period 
-          [[3]](https://www.health.gov/paguidelines). Adding in strength training twice a week can help with weight loss and 
-          increase your resting metabolic rate on top of all this!"""
+        aerobic activity (or 75 minutes of more vigorous exercise) for good general health. Depending on how intense the 
+        aerobic activity, you could take a brisk 30-min walk 5x a week or play a high-intensity sport for a shorter period 
+        [[3]](https://www.health.gov/paguidelines). Adding in strength training twice a week can help with weight loss and 
+        increase your resting metabolic rate on top of all this!"""
       },
       {
         'title': 'What are some easy things to do?',
         'content': """There are easy ways to incorporate physical activity in your day-to-day. Sitting for prolonged 
-          periods throughout the day has been linked to heart disease and diabetes, and stepping away from the chair or 
-          even just standing for a few minutes every hour can help reduce these negative effects 
-          [[4]](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3404815), [[5]](https://www.ncbi.nlm.nih.gov/pubmed/25931456). 
-          Choosing to walk or bike, taking the stairs, and cleaning your room are easy ways to incorporate more 
-          physical activity in your day."""
+        periods throughout the day has been linked to heart disease and diabetes, and stepping away from the chair or 
+        even just standing for a few minutes every hour can help reduce these negative effects 
+        [[4]](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3404815), [[5]](https://www.ncbi.nlm.nih.gov/pubmed/25931456). 
+        Choosing to walk or bike, taking the stairs, and cleaning your room are easy ways to incorporate more 
+        physical activity in your day."""
       },
       {
         'title': 'Should I focus on strength or cardio?',
         'content': """It's important to do both! Strength training is important for building muscles, preventing injury, 
-          and improving your metabolism. Cardio (aerobic exercise that elevates your heart rate) improves heart health, 
-          helps with weight loss, and increases your metabolism, too 
-          [[6]](https://health.clevelandclinic.org/2016/02/head-toe-benefits-cardio-workout-infographic)."""
+        and improving your metabolism. Cardio (aerobic exercise that elevates your heart rate) improves heart health, 
+        helps with weight loss, and increases your metabolism, too 
+        [[6]](https://health.clevelandclinic.org/2016/02/head-toe-benefits-cardio-workout-infographic)."""
       },
       {
         'title': 'If you\'re getting bored, switch it up!',
         'content': """Try mixing up your workout--varying your exercise routines will help you avoid plateaus in workout 
-          performance and also improve adherence. Figuring out what activities get you most excited and motivated will 
-          be key to getting consistent physical activity."""
+        performance and also improve adherence. Figuring out what activities get you most excited and motivated will 
+        be key to getting consistent physical activity."""
       }
     ],
     'links': [
@@ -137,10 +77,55 @@ universal_challenge_info = {
     ],
     'preview_text': 'Science has proven that even small amounts of exercise can lead to significant changes in both physical and mental health.'
   },
-  'group-selected-challenge': {
-    'defaultIndex': 6,
-    'name': 'Group-Selected Challenge',
-    'icon': '',
+  'good-deeds': {
+    'defaultIndex': 2,
+    'name': 'Good Deeds',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/good-deeds-green.png',
+    'overview': [
+      {
+        'title': '',
+        'content': ''
+      }
+    ],
+    'links': [],
+    'extra_info': '',
+    'check_in_questions': [],
+    'preview_text': 'A positive feedback loop exists between kindness and happiness &mdash; the more you do to make other people feel better, the more your own happiness increases!'
+  },
+  'healthy-eating': {
+    'defaultIndex': 3,
+    'name': 'Healthy Eating',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/healthy-eating-green.png',
+    'overview': [
+      {
+        'title': '',
+        'content': ''
+      }
+    ],
+    'links': [],
+    'extra_info': '',
+    'check_in_questions': [],
+    'preview_text': 'Healthy eating has multiple benefits for you. It can help you stay in shape, increase your energy, and boost your immune system.'
+  },
+  'journaling': {
+    'defaultIndex': 4,
+    'name': 'Journaling',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/journaling-green.png',
+    'overview': [
+      {
+        'title': '',
+        'content': ''
+      }
+    ],
+    'links': [],
+    'extra_info': '',
+    'check_in_questions': [],
+    'preview_text': 'Physically logging your days and putting your thoughts on paper can be great for organizing thoughts.'
+  },
+  'campus-selected-challenge': {
+    'defaultIndex': 5,
+    'name': 'Campus-Selected Challenge',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/group-challenge-green.png',
     'overview': [
       {
         'title': '',
@@ -152,10 +137,25 @@ universal_challenge_info = {
     'check_in_questions': [],
     'preview_text': 'Goal setting is important for your short-term motivation and long term vision. During your week off, come up with a challenge amongst your friends and see how well you can stick to it!'
   },
+  'sleep': {
+    'defaultIndex': 6,
+    'name': 'Sleep',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/sleep-green.png',
+    'overview': [
+      {
+        'title': '',
+        'content': ''
+      }
+    ],
+    'links': [],
+    'extra_info': '',
+    'check_in_questions': [],
+    'preview_text': 'We all underestimate how big of an impact getting a good night\'s sleep can have on our overall health and happiness.'
+  },
   'positivity-mindfulness': {
     'defaultIndex': 7,
     'name': 'Positivity & Mindfulness',
-    'icon': '',
+    'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/positivity-green.png',
     'overview': [
       {
         'title': '',
