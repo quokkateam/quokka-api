7253655d92260785c0feca85531a231a07c8192e
Ben Whittle <benwhittle31@gmail.com>
2017-10-03 17:48:03 -0700

    TODO comment

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ from src.helpers import auth_util
 
 
 class User(db.Model):
+  # TODO: combine email_verification_sent and email_verified into one Integer "status" column
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(120), index=True, unique=True)
   name = db.Column(db.String(120), nullable=False)
