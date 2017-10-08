3dd128b553f06554316b596c54c0ae8d6224fa69
Ben Whittle <benwhittle31@gmail.com>
2017-10-08 16:14:46 -0700

    wrote weekly email job

--- src/mailers/challenge_mailer.py
+++ src/mailers/challenge_mailer.py
@@ -1,14 +1,12 @@
 from src.mailers.client import send_email
 
 
-def weekly_challenge(school=None, delay=True):
-  # calculate week_index from today's date
-  week_index = 0
-  week_num = week_index + 1
-
+def weekly_challenge(user=None, vars={}, template_path=None, delay=True):
   return send_email(
-    to='benwhittle31@gmail.com',
-    subject='Week {} Challenge'.format(week_num),
+    to=user.email,
+    subject='Week {} Challenge'.format(vars.get('week_num')),
+    template_vars=vars,
+    template_path=template_path,
     delay=delay
   )
 
---
+++ src/tasks/send_weekly_emails.py
@@ -0,0 +1,81 @@
+from operator import attrgetter
+from src.models import School
+from src import dbi, logger
+from datetime import date
+from src.mailers.challenge_mailer import weekly_challenge
+from src.helpers.definitions import templates_dir
+
+
+# Make this run every day at X time
+def perform():
+  schools = dbi.find_all(School)
+
+  logger.info('Checking if weekly emails should be sent for {} schools...'.format(len(schools)))
+
+  for school in schools:
+    challenge_info = find_newly_started_challenge(school)
+
+    if not challenge_info:
+      logger.info('Not today -- {}'.format(school.name))
+      continue
+
+    challenge, week_num = challenge_info
+
+    logger.info('New Challenge Detected ({}) for {}...'.format(challenge.name, school.name))
+
+    users = school.active_users()
+
+    weekly_email_info = format_weekly_email_vars(challenge, week_num)
+
+    logger.info('Sending emails to {} users at {}...'.format(len(users), school.name))
+
+    template_path = '{}/challenge_mailer/weekly_challenges/{}.html'.format(templates_dir, challenge.slug)
+
+    failures = []
+    for user in users:
+      success = weekly_challenge(user, weekly_email_info, template_path=template_path, delay=False)
+
+      if not success:
+        failures.append(user.email)
+
+    logger.info('Emails Sent with {} Failures: {}'.format(len(failures), ','.join(failures)))
+
+  logger.info('DONE.')
+
+
+def find_newly_started_challenge(school):
+  challenges = sorted(school.active_challenges(), key=attrgetter('start_date'))
+
+  start_dates = [c.start_date.date() for c in challenges]
+  today = date.today()
+
+  if today not in start_dates:
+    return None
+
+  week_index = start_dates.index(today)
+  week_num = week_index + 1
+  challenge = challenges[week_index]
+
+  return challenge, week_num
+
+
+def format_weekly_email_vars(challenge, week_num):
+  prizes = []
+
+  for p in challenge.prizes:
+    sponsor = p.sponsor
+
+    prizes.append({
+      'prizeName': p.name,
+      'sponsorName': sponsor.name,
+      'sponsorLogo': sponsor.logo
+    })
+
+  return {
+    'week_num': week_num,
+    'name': challenge.name,
+    'challenge': challenge.text,
+    'suggestions': challenge.suggestions,
+    'prizes': prizes,
+    'overview': 'Challenge Overview'
+  }
