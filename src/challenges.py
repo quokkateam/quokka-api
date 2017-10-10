b1e9f502b57ddc721f99d39e06514a235c038324
Ben Whittle <benwhittle31@gmail.com>
2017-10-09 23:33:10 -0700

    updated challenges.py

--- src/challenges.py
+++ src/challenges.py
@@ -117,11 +117,38 @@ universal_challenge_info = {
     'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/good-deeds-green.png',
     'overview': [
       {
-        'title': '',
-        'content': ''
+        'title': 'Why should I do good deeds?',
+        'content': """Performing acts of kindness for other people can be beneficial for your own health, too. 
+        It promotes positive physiological changes in your brain associated with happiness, releasing endorphins 
+        that help you feel happy naturally. It creates a positive feedback loop that encourages generosity and happiness 
+        and can reduce stress/negative feelings."""
+      },
+      {
+        'title': 'What\'s covered by a "good deed?"',
+        'content': """Whether you're volunteering to help your community, doing something for a good cause, 
+        or performing random acts of kindness, doing good deeds means you're helping others without an expectation of 
+        reciprocity or recognition. Do what you can to help others, but be careful that you don't take on so much you 
+        end up burning out!"""
+      },
+      {
+        'title': 'Who should I help?',
+        'content': """Help your friends, your family, strangers, or give back to the community. Prosocial behavior 
+        towards others in general can help spread kindness. Small actions on your end can make another person's whole day!"""
+      },
+      {
+        'title': 'What are some examples?',
+        'content': """Check out [this list for ideas](https://www.randomactsofkindness.org/the-kindness-blog/2943-50-kindness-ideas-for-random-acts-of-kindness-day) of 
+        a few random acts of kindness you can do. To incorporate this type of behavior into your daily life broadly, 
+        try to keep an open eye to people who might need your help, even in small ways."""
       }
     ],
-    'links': [],
+    'links': [
+      'https://www.randomactsofkindness.org/the-science-of-kindness',
+      'https://greatergood.berkeley.edu/article/item/kindness_makes_you_happy_and_happiness_makes_you_kind',
+      'https://www.mentalhealth.org.uk/publications/doing-good-does-you-good',
+      'https://doi.org/10.1007/s10902-008-9098-1',
+      'http://www.nytimes.com/2013/03/31/magazine/is-giving-the-secret-to-getting-ahead.html'
+    ],
     'extra_info': '',
     'check_in_questions': [
       'How did you feel about this week\'s challenge?',
@@ -131,7 +158,7 @@ universal_challenge_info = {
     'preview_text': 'A positive feedback loop exists between kindness and happiness - the more you do to make other people feel better, the more your own happiness increases!',
     'email': {
       'science': '',
-      'welcome_text': ''
+      'welcome_text': 'Let\'s help others!'
     }
   },
   'healthy-eating': {
@@ -140,11 +167,61 @@ universal_challenge_info = {
     'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/healthy-eating-green.png',
     'overview': [
       {
-        'title': '',
-        'content': ''
-      }
+        'title': 'What\'s the science say?',
+        'content': """The Dietary Guidelines for Americans recommends eating a variety from the different food groups 
+        each day (fruits, vegetables, grains, protein, and dairy). The USDA also provides MyPlate to illustrate what 
+        portions of the different food groups on a plate can look like for a balanced diet. The Harvard School of Public 
+        Health's Healthy Eating Plate gives more detailed guidance towards making healthier food choices emphasizing quality."""
+      },
+      {
+        'title': 'What are some basic rules of thumb?',
+        'content': """Try to balance out your meal without worrying too much about the small details. It's helpful to 
+        be informed about the Dietary Guidelines and MyPlate recommendations, but it's more important to find an eating 
+        pattern that keeps you full, confident in the healthy food decisions you make, and enjoying the foods you're 
+        eating. Basic guidelines suggest aiming for meals with 50% fruits and vegetables, 25% proteins, and 25% grains. 
+        Beyond that, we could all drink more water and eat more fiber (berries, broccoli, and beans are all rich in this!)."""
+      },
+      {
+        'title': 'Does eating breakfast actually matter?',
+        'content': """Eating earlier on in the day kickstarts your metabolism. Research has shown that morning eating 
+        can decrease binge eating later in the day and help you maintain a stable weight. If you typically skip the first 
+        meal of the day, try starting with something easy and small like a banana, yogurt, or high protein/high fiber 
+        "meal bar" that doesn't have too much sugar."""
+      },
+      {
+        'title': 'Should I eat 3 big meals or 5 smaller meals?',
+        'content': """It's often recommended to eat smaller, frequent meals every 3-4 hours to keep your metabolism and 
+        energy levels up. This could be 3 full meals with 2 smaller snacks or steady portions throughout the day. Either 
+        way, you just want to make sure you give your brain and body a regular level of energy with your food intake 
+        throughout the day."""
+      },
+      {
+        'title': 'Is snacking good or bad?',
+        'content': """Snacks can be important to boost energy between meals and decrease interest in late-day binge 
+        eating. Snacking itself isn't inherently good or bad - it depends on what you choose to eat between meals. 
+        Sometimes, we resort to snacking because we're bored or dehydrated rather than actually hungry. But if it's 
+        been a few hours since you last ate, try to snack consciously and pick a nutrient-rich option like baby 
+        carrots, fresh fruit, yogurt, or nuts and peanut butter."""
+      },
+      {
+        'title': 'What are some foods that help me: stay awake, stay healthy, stay focused?',
+        'content': """There are plenty of different foods that can boost your daily activity needs, whether that's 
+        staying awake or staying focused. Maintaining your metabolism is important, so try to eat regularly throughout 
+        the day to get consistent energy. Foods with omega-3 fats, antioxidants, and fiber (e.g., blueberries, avocados, 
+        green leafy vegetables, dark chocolate, oatmeal) have been associated with improved cognitive performance, 
+        memory, and concentration. """
+      },
     ],
-    'links': [],
+    'links': [
+      'https://health.gov/dietaryguidelines/',
+      'https://www.fns.usda.gov/tn/myplate',
+      'https://www.hsph.harvard.edu/nutritionsource/healthy-eating-plate/',
+      'https://www.choosemyplate.gov/myplate-tip-sheets',
+      'https://www.hsph.harvard.edu/nutritionsource/what-should-you-eat/',
+      'https://cspinet.org/nutrition-tips',
+      'https://healthyforgood.heart.org/eat-smart',
+      'http://www.huffingtonpost.com/2015/02/23/brain-food-superfoods_n_1895328.html',
+    ],
     'extra_info': '',
     'check_in_questions': [
       'How did you feel about this week\'s challenge?',
@@ -232,11 +309,68 @@ universal_challenge_info = {
     'icon': 'https://s3-us-west-1.amazonaws.com/quokkadev/images/positivity-green.png',
     'overview': [
       {
-        'title': '',
-        'content': ''
+        'title': 'What\'s mindfulness?',
+        'content': """Mindfulness is often described as paying attention to the present moment with purpose and openness. 
+        Our minds are often lost in thought and can be easily distracted, particularly with an emphasis on dwelling in 
+        the past or wondering about the future. Meditation is often used as a best practice for mindfulness because it 
+        emphasizes slowing down, breathing, and observing inner experiences."""
+      },
+      {
+        'title': 'What are the benefits of mindfulness?',
+        'content': """Research says that focusing our minds on the present moment and being aware of our thoughts, 
+        feelings, and actions has many health benefits. One popular technique includes mindfulness-based stress reduction, 
+        which can be a useful tool in managing medical and psychological conditions like anxiety, sleep issues, and chronic pain."""
+      },
+      {
+        'title': 'How might meditation in particular help me?',
+        'content': """Meditation is defined as the practice of engaging in mental exercise (such as concentration on 
+        one's breathing or repetition of a mantra) for the purpose of reaching a heightened level of awareness of the 
+        present moment and of spiritual growth. Meditation has been associated with increased focus and learning 
+        concentration, improved memory and attention span, and greater psychological resilience."""
+      },
+      {
+        'title': 'What are the benefits to thinking positively?',
+        'content': """Because it's easy to dwell in the past or worry about the future, sometimes negative thoughts 
+        can cloud our minds. There has been a lot of research that supports the effects of positive thinking and 
+        optimism on health, including increased life span, lower rates of depression, and better coping skills with 
+        stressful situations."""
+      },
+      {
+        'title': 'What is positive vs. negative thinking?',
+        'content': """While positive vs. negative thinking is often just broken down to optimism against pessimism, 
+        it may be more helpful to see examples of what differences there are. Negative thinking includes filtering 
+        (where you magnify negative aspects of a situation and filter out the positive ones), personalizing (where 
+        you blame yourself for a bad situation occurring), catastrophizing (anticipating the worst to occur), and 
+        polarizing (seeing things as either good or bad, with no middle ground)."""
+      },
+      {
+        'title': 'What are some easy ways to focus on positive thinking?',
+        'content': """To engage in positive thinking, you can identify areas of life that you usually think 
+        negatively about, evaluate why your thoughts are that way, and practice reframing these situations in 
+        a positive light. Be aware of when negative thoughts enter your mind and respond with affirmations towards 
+        yourself. Be encouraging and think about the things you are thankful for. If you want to build resiliency 
+        to stressful situations, try having something to look forward to when you wake up every day (this could be 
+        as simple as finishing a task, seeing a friend, or trying something different)."""
+      },
+      {
+        'title': 'Is it ok if I just acknowledge my negative thinking patterns?',
+        'content': """Of course. The goal is not to deceive yourself into thinking that you're happy. Don't force 
+        yourself into being constantly positive -- start by acknowledging negative emotions, then practice reframing 
+        your perspective over time."""
       }
     ],
-    'links': [],
+    'links': [
+      'http://science.sciencemag.org/content/330/6006/932.full',
+      'https://wellness.huhs.harvard.edu/Mindfulness',
+      'https://www.umassmed.edu/cfm/mindfulness-based-programs/mbsr-courses/about-mbsr/mbsr-journal-articles/',
+      'http://jamanetwork.com/journals/jamainternalmedicine/fullarticle/1809754',
+      'http://www.cumc.columbia.edu/student-health/center-student-wellness/wellness-resources/mindfulnessmeditation',
+      'https://news.harvard.edu/gazette/story/2016/12/optistic-women-live-longer-are-healthier/',
+      'https://academic.oup.com/aje/article-abstract/185/1/21/2631298/Optimism-and-Cause-Specific-Mortality-A?sid=7816bd15-60fd-491b-bd99-f2cd39646d10',
+      'http://www.mayoclinic.org/healthy-lifestyle/stress-management/in-depth/positive-thinking/art-20043950',
+      'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3156028/',
+      'http://www.hopkinsmedicine.org/health/healthy_aging/healthy_mind/the-power-of-positive-thinking'
+    ],
     'extra_info': '',
     'check_in_questions': [
       'How did you feel about this week\'s challenge?',
@@ -246,7 +380,7 @@ universal_challenge_info = {
     'preview_text': 'Thinking positively and being mindful of one\'s emotions are great habits for anyone to develop during his or her long-term pursuit of happiness.',
     'email': {
       'science': '',
-      'welcome_text': ''
+      'welcome_text': 'Let\'s focus on our thoughts!'
     }
   }
 }
