8ba16b344fadc84f6e0d27d399a8a557e619c97c
Ben Whittle <benwhittle31@gmail.com>
2017-10-12 13:01:15 -0700

    fixing prize winner selection

--- src/routes/winner.py
+++ src/routes/winner.py
@@ -6,8 +6,9 @@ from src.helpers.winner_helper import formatted_winners
 from src import dbi
 from operator import attrgetter
 from datetime import date
-from src.models import Winner, User, Prize
+from src.models import Winner, User, Prize, CheckInAnswer
 from src.mailers import challenge_mailer
+from random import shuffle
 
 choose_winners_model = api.model('Winner', {
   'challenge_id': fields.Integer(required=True)
@@ -33,64 +34,95 @@ class RestfulWinners(Resource):
   @namespace.doc('choose_winners_for_challenge')
   @namespace.expect(choose_winners_model, validate=True)
   def post(self):
+    # NOTE: Very aware this endpoint makes a shit-ton of queries and most everything should
+    # be eager loaded, but I don't have time to figure all that syntax bullshit out right now for
+    # properly eager loading. TODO: Refactor this
     user = current_user()
 
     if not user or not user.is_admin:
       return '', 403
 
     school = user.school
-    school_challenges = sorted(school.active_challenges(), key=attrgetter('start_date'))
-    challenge = [c for c in school_challenges if c.id == api.payload['challenge_id']]
+    challenges = sorted(school.active_challenges(), key=attrgetter('start_date'))
 
+    # Get challenge to assign winners for
+    challenge = [c for c in challenges if c.id == api.payload['challenge_id']]
+
+    # Make sure challenge exists...
     if challenge:
       challenge = challenge[0]
     else:
       return 'Challenge Not Found', 404
 
+    # If challenge hasn't started yet, error out
     if challenge.start_date.date() > date.today():
       return '', 401
 
+    # Get prizes for this challenge
     prizes = sorted(challenge.prizes, key=attrgetter('id'))
+    prize_ids = [p.id for p in prizes]
 
-    # Make sure winners haven't already been chosen
-    challenge_winners = dbi.find_all(Winner, {
-      'prize_id': [p.id for p in prizes]
-    })
-
-    if challenge_winners:
+    # Error out if winners have already been chosen for this challenge
+    if dbi.find_all(Winner, {'prize_id': prize_ids}):
       return 'Winners already chosen for this challenge', 500
 
-    all_challenge_prizes = dbi.find_all(Prize, {
-      'challenge_id': [c.id for c in school_challenges]
+    # Get reference to all prizes across all challenges for this school
+    school_prizes = dbi.find_all(Prize, {
+      'challenge_id': [c.id for c in challenges]
     })
 
+    # Get list of all past winners for this school
     past_winners = []
-    for p in all_challenge_prizes:
+    for p in school_prizes:
       past_winners += p.winners
 
-    past_winner_users = dbi.find_all(User, {'id': [w.user_id for w in past_winners]})
+    # Find user_ids for all past winners
+    past_winner_user_ids = [u.id for u in dbi.find_all(User, {'id': [w.user_id for w in past_winners]})]
 
-    potential_winner_user_ids = [u.id for u in school.users if u.id not in past_winner_users]
+    # Get all check_in_question_ids for this check_in and sort them
+    question_ids = [q.id for q in challenge.check_in.check_in_questions]
+    question_ids.sort()
+
+    # Find ALL check_in_answers related to these check_in_questions
+    answers = dbi.find_all(CheckInAnswer, {'check_in_question_id': question_ids})
+
+    # Create a map of user_id to list of check_in_question_ids that they answered for this check_in
+    # Ex: {1: [2, 3, 4]} --> we'll then compare [2, 3, 4] to question_ids to see if they match
+    user_ids_to_question_ids = {}
+    for a in answers:
+      if a.user_id not in user_ids_to_question_ids:
+        user_ids_to_question_ids[a.user_id] = [a.check_in_question_id]
+      else:
+        user_ids_to_question_ids[a.user_id].append(a.check_in_question_id)
+
+    # Find users who:
+    # (1) haven't been selected as winners in the past
+    # (2) have answered all check_in_questions for this check_in
+    potential_winner_user_ids = []
+    for k, v in user_ids_to_question_ids.items():
+      v.sort()
+      if k not in past_winner_user_ids and v == question_ids:
+        potential_winner_user_ids.append(k)
 
     if not potential_winner_user_ids:
-      return 'Everyone has already won!', 501
+      return 'No users currently eligible for prizes -- either everyone has already won or no one has filled out check-ins', 400
 
-    potential_winners = dbi.find_all(User, {'id': potential_winner_user_ids})
+    potential_winning_users = dbi.find_all(User, {'id': potential_winner_user_ids})
 
     prize_ids_for_winners = []
     for p in prizes:
       prize_ids_for_winners += ([p.id] * p.count)
 
-    winning_users = random_subset(potential_winners, len(prize_ids_for_winners))
+    # Random prize assignment
+    shuffle(prize_ids_for_winners)
+
+    winning_users = random_subset(potential_winning_users, len(prize_ids_for_winners))
 
     i = 0
     for u in winning_users:
-      prize_id = prize_ids_for_winners[i]
-
-      # Create the winner
       winner = dbi.create(Winner, {
         'user': u,
-        'prize_id': prize_id
+        'prize_id': prize_ids_for_winners[i]
       })
 
       # Send the winner an email congratulating him/her
@@ -98,6 +130,6 @@ class RestfulWinners(Resource):
 
       i += 1
 
-    data = formatted_winners(school_challenges)
+    data = formatted_winners(challenges)
 
     return data
