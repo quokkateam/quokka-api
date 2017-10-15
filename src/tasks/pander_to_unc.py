95c9a9c2af36a5cb6bcb7d0981747a12468da953
Ben Whittle <benwhittle31@gmail.com>
2017-10-14 19:40:24 -0700

    modifying pander_to_unc to create their check_in_questions

--- src/tasks/pander_to_unc.py
+++ src/tasks/pander_to_unc.py
@@ -1,37 +1,35 @@
 from src.models import *
 from src import dbi
+from src.challenges import universal_challenge_info
 
+school = dbi.find_one(School, {'slug': 'unc'})
 
-def destroy_records(records):
-  [dbi.destroy(r) for r in records]
+challenges = school.active_challenges()
 
-print('Changing names to Self-Care...')
-# Change name of 'Campus-Selected Challenge' to 'Self-Care'
-cs_challenges = dbi.find_all(Challenge, {'slug': 'campus-selected-challenge'})
-[dbi.update(c, {'name': 'Self-Care'}) for c in cs_challenges]
+for challenge in challenges:
+  check_in = challenge.check_in
 
-unc = dbi.find_one(School, {'slug': 'unc'})
+  if not check_in:
+    continue
 
-if not unc:
-  print('No School with slug, "unc".')
-  exit()
+  if len(check_in.check_in_questions) > 0:
+    continue
 
-challenges = [c for c in unc.challenges if c.slug in ['good-deeds', 'journaling']]
+  universal_challenge = universal_challenge_info.get(challenge.slug)
 
-# Destroy the challenges (and dependent models through challenge) that UNC doesn't want
-for c in challenges:
-  print('Destroying appropriate records for {} challenge...'.format(c.slug))
+  if not universal_challenge:
+    continue
 
-  check_in = c.check_in
-  check_in_questions = check_in.check_in_questions
-  check_in_answers = []
+  questions = universal_challenge['check_in_questions']
 
-  for q in check_in_questions:
-    check_in_answers += q.check_in_answers
+  i = 0
+  for q in questions:
+    print('Creating CheckInQuestion...')
 
-  destroy_records(c.prizes)
-  destroy_records(check_in_answers)
-  destroy_records(check_in_questions)
+    dbi.create(CheckInQuestion, {
+      'check_in': check_in,
+      'text': q,
+      'order': i
+    })
 
-  dbi.destroy(check_in)
-  dbi.destroy(c)
+    i += 1
